1b73f4122493f3716fde1c7ffe5a2e53687c0e1d
NetanelWitkin <witkinn@post.bgu.ac.il>
2025-04-16 18:39:07 +0300

    basic impl: constructer,getters and setters

---
+++ worshop-project/src/Domain/Shop.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+
+public class Shop {
+
+    public int id;
+    public String name;
+    public String description;
+    private int founderId;
+    public PurchasePolicy purchasePolicy;
+    public DiscountPolicy discountPolicy;
+    public HashMap<Integer, Integer> items;
+    private boolean isOpen;
+
+    public Shop(int id, String name, String description, int founderId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.founderId = founderId;
+        this.purchasePolicy = new PurchasePolicy();
+        this.discountPolicy = new DiscountPolicy();
+        this.items = new HashMap<>();
+        this.isOpen = false;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public int getFounderId() { return founderId; }
+    public boolean isOpen() { return isOpen; }
+    public PurchasePolicy getPurchasePolicy() { return purchasePolicy; }
+    public DiscountPolicy getDiscountPolicy() { return discountPolicy; }
+    public HashMap<Integer, Integer> getItems() { return items; }
+
+    public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
+    public void setPurchasePolicy(PurchasePolicy purchasePolicy) { this.purchasePolicy = purchasePolicy; }
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) { this.discountPolicy = discountPolicy; }
+    public void setOpen(boolean isOpen) { this.isOpen = isOpen; }
+
+}
+
