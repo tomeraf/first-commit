123cf063711bbf42789927ec965035dac328e126
afekco <afekco@post.bgu.ac.il>
2025-04-22 00:40:31 +0300

    update Irole

--- worshop-project/src/Domain/IRole.java
+++ worshop-project/src/Domain/IRole.java
@@ -4,25 +4,15 @@ import java.util.List;
 import java.util.Map;
 public interface IRole {
 
+    boolean hasPermission(Permission permission);
+    void addPermission(Permission permission);
+    void removePermission(Permission permission);
 
-   // Map<List<String>, Map<String, List<Permissions>>> getShopMembersInfo(String shopIds);//<owners, <managers, permissions>>
+    public void AddAppointment(int nomineeID);
+    public void RemoveAppointment(int appointeeID);
 
-    // // void respondToMessage(String shopId, String userId, String message);
-    // // String viewPurchaseHistory(String shopId);
-
-    // // void updatePurchasePolicy(String shopId, String policyId);
-    // // void updateDiscountPolicy(String shopId, String policyId);
-
-    void addItem(Shop shop , String name, Category category, double price);//shop.addItem
-    void removeItem(int itemId);//badket.removeItem
-
-    void updatItemName(Shop shop, String itemId, String itemName);
-    void updateItemPrice(Shop shop, String itemId, double itemPrice);
-    void updateItemQuantity(Shop shop, String itemId, int itemQuantity);//shop.
-
-    public void AddManager(Shop shop, int managerID);
-    public void AddOwner(Shop shop, int ownerID);
-    public void UpdateManagerPermissions(Shop shop, List<Permission> authorizations);
-    public void RemoveOwner(Shop shop, int ownerIdToRemove);
-    public void RemoveManager(Shop shop, int managerIdtoRemove);
+    List<Integer> getAppointments(); // Returns a list of all the appointments the role has made  
+    int getAppointer();
+    List<Permission> getPermissions();
+    int getShopID();
 }
