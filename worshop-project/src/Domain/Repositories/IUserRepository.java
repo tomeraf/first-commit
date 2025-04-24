c8ff96afc503748cb728d4f5958b1e038f4b5b81
LaviOssy1 <ossy@post.bgu.ac.il>
2025-04-24 22:50:53 +0300

    Finished all non-dependent methods, waiting for others to have complete services

--- worshop-project/src/Domain/Repositories/IUserRepository.java
+++ worshop-project/src/Domain/Repositories/IUserRepository.java
@@ -2,6 +2,7 @@ package Domain.Repositories;
 
 import Domain.Item;
 import Domain.Registered;
+import Domain.ShoppingCart;
 import Domain.DTOs.UserDTO;
 import Domain.DTOs.UserRoleDTO;
 
@@ -10,14 +11,24 @@ import java.util.List;
 
 public interface IUserRepository {
     void saveUser(Registered user);
-    Registered getUserById(int id);
     void updateUser(Registered user);
     void deleteUser(int id);
+    
+    Registered getUserById(int id);
+
+    int getIdToAssign(); // It will give a unique ID for the user
+    boolean removedId(int id);
+    
     boolean isUsernameExists(String username);
     HashMap<Integer,Registered> getAllUsers();
     void assignRoleToUserInShop(int userId, int shopId, String role);
     String getRoleOfUserInShop(int userId, int shopId);
-    int getIdToAssign();
-    boolean removedId(int id);
     void saveCartContent(int cartID, List<Item> items); // middle-table
+
+    ShoppingCart getShoppingCart(int cartID);
+    void removedCartContent(int userID);
+
+    void addItemsToCart(int cartID, List<Integer> itemIDs, List<Integer> shopIDs); // save the cart in the DB
+    void removeItemsFromCart(int cartID, List<Integer> itemIDs, List<Integer> shopIDs); // remove the items from the cart in the DB
+    Registered getUserByName(String username);
 }
--- worshop-project/src/Service/UserService.java
+++ worshop-project/src/Service/UserService.java
@@ -14,6 +14,7 @@ import org.junit.platform.commons.logging.LoggerFactory;
 
 import Domain.Guest;
 import Domain.Registered;
+import Domain.ShoppingCart;
 import Domain.Adapters_and_Interfaces.JWTAdapter;
 import Domain.DTOs.ItemDTO;
 import Domain.DTOs.UserDTO;
@@ -40,113 +41,175 @@ public class UserService {
     public String enterToSystem() {
         logger.info(() -> "User entered the system");
         Guest guest = new Guest();
-        guest.enterToSystem(userRepository.getTotalUsersCount()); 
-        userRepository.incTotalUsersCount();
-        return jwtAdapter.generateToken(guest.getCart().getCartID()+"");
-
+        // Shopping cart is created here, it seems redundant but then we need it to get the ID of the cart(UserID == CartID)
+        guest.enterToSystem(userRepository.getIdToAssign()); 
+        return jwtAdapter.generateToken(guest.getUserID()+"");
     }
 
-    public void exit(String sessionToken) {
+    public void exitAsGuest(String sessionToken) {
         try {
             if (!jwtAdapter.validateToken(sessionToken)) {
-                throw new Exception("User not logged in");
+                throw new Exception("User is not logged in");
             }
-            String username = jwtAdapter.getUsername(sessionToken);
-            Guest guest = new Guest();
-            registered.logout();
+            int userID = Integer.parseInt(jwtAdapter.getUsername(sessionToken));
+            userRepository.removedCartContent(userID); // User' ID and its cart's ID are same - it goes to the table <cartID, itemID> and removes
+            userRepository.removedId(userID); // Adds to the "reuse" list
+            logger.info(() -> "User exited the system");
         } catch (Exception e) {
             logger.error(() -> "Error exiting the system: " + e.getMessage());
         }
-        logger.info(() -> "User exited the system");
-        userRepository.decGuestsCount();
-
     }
-    
-    public void registerUser(String username, String password, LocalDate dateOfBirth) {
+
+    // After logout - the user remains in the system, as guest
+    public String logout(String sessionToken) {
         try {
-            if (userRepository.getUserByName(username) != null) {
+            if (!jwtAdapter.validateToken(sessionToken)) {
+                throw new Exception("User is not logged in");
+            }
+            // Nothing to do, everything is saved in DB
+            return enterToSystem();
+        } catch (Exception e) {
+            logger.error(() -> "Logout Error: " + e.getMessage());
+            return "";
+        }
+    }
+
+    public void exitAsRegistered(String sessionToken) {
+        try {
+            if (!jwtAdapter.validateToken(sessionToken)) {
+                throw new Exception("User is not logged in");
+            }
+            String guestSessionToken = logout(sessionToken); // Makes the registered to be guest
+            exitAsGuest(guestSessionToken); // and the exit as guest
+            
+            // I don't know if isInSession is going to be stored in the DB, currently, everything remains
+            
+            logger.info(() -> "User exited the system");
+        } catch (Exception e) {
+            logger.error(() -> "Error exiting the system: " + e.getMessage());
+        }
+    }
+
+    public void registerUser(String sessionToken, String username, String password, LocalDate dateOfBirth) {
+        try {
+            if (!jwtAdapter.validateToken(sessionToken)) {
+                throw new Exception("User is not logged in");
+            }
+            int idToAssign = userRepository.getIdToAssign();
+            if (userRepository.getUserById(idToAssign) != null) {
                 throw new Exception("Username already exists");
             }
+            int currentGuestUserID = Integer.parseInt(jwtAdapter.getUsername(sessionToken));
+            ShoppingCart cart = userRepository.getShoppingCart(currentGuestUserID);
             Guest guest = new Guest();
+            
+            // + MUST MODIFY GUEST TO HAVE SESSION_TOKEN INSTEAD OF "IS_IN_SESSION"
+            guest.setSessionToken(sessionToken); // Set the session token for the guest
+            guest.setCart(cart);
+            // Stays with same token
             Registered registered = guest.register(username, password, dateOfBirth);
             userRepository.saveUser(registered);
         } catch (Exception e) {
             logger.error(() -> "Error registering user: " + e.getMessage());
         }
-
     }
-    //return session token
-    public String loginUser(String username, String password) {
+
+    // Returns session token
+    public String loginUser(String sessionToken, String username, String password) {
         try {
+            if (!jwtAdapter.validateToken(sessionToken)) {
+                throw new Exception("User is not logged in");
+            }
             Registered registered = userRepository.getUserByName(username);
+            
             //should throw exception if user not found in the repository
             if (!registered.getPassword().equals(password)) {
                 throw new Exception("Username and password do not match");
             }
-            String sessionToken = jwtAdapter.generateToken(username);
-            userRepository.decGuestsCount();
+
+            int guestUserID = Integer.parseInt(jwtAdapter.getUsername(sessionToken));
+            String newSessionToken = jwtAdapter.generateToken(guestUserID+"");
+            
             logger.info(() -> "User logged in successfully with session token: " + sessionToken);
             return sessionToken;
         } catch (Exception e) {
             logger.error(() -> "Error logging in user: " + e.getMessage());
+            return null;
         }
-        return null;
     }
-    
-    public List<ItemDTO> viewCart(String sessionToken) {
+
+    public List<ItemDTO> checkCartContent(String sessionToken) {
         try {
             if (!jwtAdapter.validateToken(sessionToken)) {
                 throw new Exception("User not logged in");
             }
-            String username = jwtAdapter.getUsername(sessionToken);
-            Registered registered = userRepository.getUserByName(username);
-            return registered.getCart().getItems(); 
+            int cartID = Integer.parseInt(jwtAdapter.getUsername(sessionToken));
+            ShoppingCart cart = userRepository.getShoppingCart(cartID);
+            List<ItemDTO> itemList = cart.getItems(); // Get all items in the cart
+            
+            logger.info(() -> "All items were listed successfully");
+            return itemList;
         } 
         catch (Exception e) {
             logger.error(() -> "Error viewing cart: " + e.getMessage());
+            return null;
         }
-
     }
-    
-    public void buyCartContent(String sessionToken, int cartID) {
+
+    public void addItemsToCart(String sessionToken, List<ItemDTO> items) {
         try {
             if (!jwtAdapter.validateToken(sessionToken)) {
                 throw new Exception("User not logged in");
             }
-            String username = jwtAdapter.getUsername(sessionToken);
-            Registered registered = userRepository.getUserByName(username);
+            int cartID = Integer.parseInt(jwtAdapter.getUsername(sessionToken));
+            //ShoppingCart cart = userRepository.getShoppingCart(cartID);
+            //boolean success = cart.addItems(items); // Add items to the cart
+
+            List<Integer> itemIDs = items.stream().map(ItemDTO::getItemID).toList(); // Extract item IDs from the list of items
+            List<Integer> shopIDs = items.stream().map(ItemDTO::getShopId).toList(); // Extract shop IDs from the list of items
+            
+            userRepository.addItemsToCart(cartID, itemIDs, shopIDs); // Save the updated cart to the repository
+            
+            logger.info(() -> "Items added to cart successfully");
+        } catch (Exception e) {
+            logger.error(() -> "Error adding items to cart: " + e.getMessage());
+        }
+    }
+
+    public void removeItemsFromCart(String sessionToken, List<ItemDTO> items) {
+        try {
+            if (!jwtAdapter.validateToken(sessionToken)) {
+                throw new Exception("User not logged in");
+            }
+            int cartID = Integer.parseInt(jwtAdapter.getUsername(sessionToken));
+            //ShoppingCart cart = userRepository.getShoppingCart(cartID);
+            //boolean success = cart.removeItems(items); // Remove items from the cart
+            List<Integer> itemIDs = items.stream().map(ItemDTO::getItemID).toList(); // Extract item IDs from the list of items
+            List<Integer> shopIDs = items.stream().map(ItemDTO::getShopId).toList(); // Extract shop IDs from the list of items
+            
+            userRepository.removeItemsFromCart(cartID, itemIDs, shopIDs); // Save the updated cart to the repository
+            
+            logger.info(() -> "Items removed from cart successfully");
+        } catch (Exception e) {
+            logger.error(() -> "Error removing items from cart: " + e.getMessage());
+        }
+    }
+
+    public void buyCartContent(String sessionToken) {
+        try {
+            if (!jwtAdapter.validateToken(sessionToken)) {
+                throw new Exception("User not logged in");
+            }
+            int cartID = Integer.parseInt(jwtAdapter.getUsername(sessionToken));
+            ShoppingCart cart = userRepository.getShoppingCart(cartID);
+
             //purchaseService.buyCartContent(registered.getCart(), cartID);
+            
+            removeItemsFromCart(sessionToken, cart.getItems()); // Remove items from the cart after purchase
+
             logger.info(() -> "Purchase completed successfully for cart ID: " + cartID);
         } catch (Exception e) {
             logger.error(() -> "Error buying cart content: " + e.getMessage());
         }
-        
     }
-
-    public void logoutUser(String sessionToken) {
-        try {
-            if (!jwtAdapter.validateToken(sessionToken)) {
-                throw new Exception("User not logged in");
-            }
-            String username = jwtAdapter.getUsername(sessionToken);
-            Registered registered = userRepository.getUserByName(username);
-            registered.logout();
-            logger.info(() -> "User logged out successfully");
-        } catch (Exception e) {
-            logger.error(() -> "Error logging out user: " + e.getMessage());
-        }
-    }
-
-    public void sendMessageToShop(String sessionToken, int shopID, String message) {
-        // Check if the user is logged in
-        // If not, prompt to log in or register
-        // If logged in, send a message to the shop with the provided details
-    }
-    public void shopOwnerRespondsToMessage(String sessionToken, int shopID, String message) {
-        //UserDTO appointer = userRepository.getUserById(appointerID
-    }
-    
-    //     public void shopPurchaseHistory(String sessionToken, int shopID) {
-    //     //UserDTO appointer = userRepository.getUserById(appointerID
-    // }
 }
