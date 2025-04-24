fbeb292370a8d9ee9451d8bf4698a53c8d7f80aa
NetanelWitkin <witkinn@post.bgu.ac.il>
2025-04-24 15:06:45 +0300

    implement some functions

--- worshop-project/src/Service/ShopService.java
+++ worshop-project/src/Service/ShopService.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import Domain.Category;
 import Domain.ItemDTO;
+import Domain.Registered;
 import Domain.Shop;
+import Domain.Adapters_and_Interfaces.IAuthentication;
+import Domain.Adapters_and_Interfaces.JWTAdapter;
 import Domain.DTOs.ShopDTO;
+import Domain.DTOs.UserDTO;
 import Domain.DomainServices.ManagementService;
 import Domain.Repositories.IShopRepository;
 import Domain.Repositories.IUserRepository;
+import jdk.vm.ci.code.Register;
 
 public class ShopService {
     
@@ -17,11 +23,13 @@ public class ShopService {
     private IShopRepository shopRepository;
     private ManagementService managementService = ManagementService.getInstance();
     private int shopIdCounter = 1;
+    private IAuthentication authenticationAdapter;
     
 
-    public ShopService(IUserRepository userRepository, IShopRepository shopRepository) {
+    public ShopService(IUserRepository userRepository, IShopRepository shopRepository, IAuthentication authenticationAdapter) {
         this.userRepository = userRepository;
         this.shopRepository = shopRepository;
+        this.authenticationAdapter = authenticationAdapter;
     }
     
     public List<ShopDTO> showAllShops() {
@@ -66,14 +74,33 @@ public class ShopService {
         return shop;
     }
 
-    public void getShopInfo() {
-
+    public ShopDTO getShopInfo(String sessionToken, int shopID) {
+        if(!authenticationAdapter.validateToken(sessionToken)){
+            System.out.println("Please log in or register to add items to the shop.");
+            return;
+        }
+        else {
+            ShopDTO shopDto = this.shopRepository.getShopById(shopID);
+            return shopDto;
+        }
     }
 
-    public void addItemToShop(String sessionToken, int shopID, String itemName, String itemDescription, double itemPrice) {
-        // Check if the user is logged in
+    public void addItemToShop(String sessionToken, int shopID, String itemName, Category category, double itemPrice) {
+        //need to add the Check if the user is logged in
         // If not, prompt to log in or register
         // If logged in, add the item to the shop with the provided details
+        if(!authenticationAdapter.validateToken(sessionToken)){
+            System.out.println("Please log in or register to add items to the shop.");
+            return;
+        }
+        else {
+            String userName = this.authenticationAdapter.getUsername(sessionToken);
+            UserDTO userDto = this.userRepository.getUserByName(userName);
+            Registered registeredUser = convertToObject(userDto);
+            ShopDTO shopDto = this.shopRepository.getShopById(shopID);
+            Shop shop = convertToObject(shopDto);
+            this.managementService.addItemToShop(registeredUser, shop, itemName, category, itemPrice);
+        }
     }
     public void removeItemFromShop(String sessionToken, int shopID, int itemID) {
         // Check if the user is logged in
@@ -84,6 +111,18 @@ public class ShopService {
         // Check if the user is logged in
         // If not, prompt to log in or register
         // If logged in, change the item quantity in the shop with the provided details
+        if(!authenticationAdapter.validateToken(sessionToken)){
+            System.out.println("Please log in or register to add items to the shop.");
+            return;
+        }
+        else {
+            String userName = this.authenticationAdapter.getUsername(sessionToken);
+            UserDTO userDto = this.userRepository.getUserByName(userName);
+            Registered registeredUser = convertToObject(userDto);
+            ShopDTO shopDto = this.shopRepository.getShopById(shopID);
+            Shop shop = convertToObject(shopDto);
+            managementService.updateItemQuantity(registeredUser, shop, itemID, newQuantity);
+        }
     }
     public void changeItemPriceInShop(String sessionToken, int shopID, int itemID, double newPrice) {
         // Check if the user is logged in
