670e2f81ddf8b9eb9e6a385c7838b90033c22733
TomerAF <tomeraf@post.bgu.ac.il>
2025-04-24 18:22:04 +0300

    added imports to shop

--- worshop-project/src/Domain/DomainServices/PurchaseService.java
+++ worshop-project/src/Domain/DomainServices/PurchaseService.java
@@ -1,4 +1,13 @@
 package Domain.DomainServices;
 
+import Domain.Guest;
+
 public class PurchaseService {
+    public void PurchaseProcess(Guest user, int CartID ){
+        if(user.getCart().getCartID()!=CartID)
+            System.out.println("Current cart is different from the cart you wish to purchase");
+
+
+    }
+
 }
