c1af0fa24693ae1f4b05c80b9f4a0e68df16e551
Tomer Cohen <128282969+tomeraf@users.noreply.github.com>
2025-04-26 20:28:09 +0300

    Delete worshop-project/src directory

--- worshop-project/src/Tests/AcceptenceTest/UserTest.java
+++
@@ -1,9 +0,0 @@
-package Tests.AcceptenceTest;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-public class UserTest
-{
-
-}
