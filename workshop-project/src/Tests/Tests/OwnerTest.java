aba186be2a6fa96d5f1cf82fa99420cfbea68bb9
afekco <afekco@post.bgu.ac.il>
2025-04-27 20:12:55 +0300

    fix owner test
    need to fix remove appointment

--- workshop-project/src/Tests/Tests/OwnerTest.java
+++ workshop-project/src/Tests/Tests/OwnerTest.java
@@ -1,60 +1,63 @@
-// package Tests;
+package Tests;
 
-// import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 
-// import java.util.HashSet;
-// import java.util.List;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
-// import org.junit.Test;
-// import org.junit.jupiter.api.BeforeEach;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
-// import Domain.Owner;
-// import Domain.Permission;
+import Domain.IRole;
+import Domain.Manager;
+import Domain.Owner;
+import Domain.Permission;
 
-// public class OwnerTest {
-//     private Owner owner;       
-//      List<Integer> appointments;
+public class OwnerTest {
+    private Owner owner;       
+     Map<Integer,IRole> appointments;
 
-//     @BeforeEach
-//     public void setUp() {
-//         // Create a Owner object with appointerID, shopID, and permissions
-//         int appointerID = -1; // -1 for founder
-//         int shopID = 101;
-//         owner = new Owner(appointerID, shopID);
-//         appointments = owner.getAppointments();
-//     }
-//     @Test
-//     public void testHasPermission() {
-//         assertTrue(owner.hasPermission(Permission.UPDATE_ITEM_QUANTITY));
-//     }
-//     @Test
-//     public void testRemoveNonExistentPermission() {
-//         // Trying to remove a permission that doesn't exist should not change anything
-//         owner.removePermission(Permission.VIEW);
-//         assertFalse(owner.hasPermission(Permission.VIEW));
-//     }
-//     @Test
-//     public void testAddAppointment() {
-//         owner.addAppointment(2);
-//         assertTrue(owner.getAppointments().contains(2));
-//     }
-//     @Test
-//     public void testRemoveAppointment() {
-//         appointments.add(2);
-//         owner.removeAppointment(2);
-//         assertFalse(owner.getAppointments().contains(2));
-//     }
-//     @Test
-//     public void testGetAppointments() {
-//         appointments.add(2);
-//         assertTrue(appointments.contains(2));
-//     }
-//     @Test
-//     public void testGetAppointer() {
-//         assertEquals(-1, owner.getAppointer());
-//     }
-//     @Test
-//     public void testGetShopID() {
-//         assertEquals(101, owner.getShopID());
-//     }
-// }
+    @Test
+    public void testHasPermission() {
+        int appointerID = -1; // -1 for founder
+        int shopID = 101;
+        owner = new Owner(appointerID, shopID);
+        appointments = owner.getAppointments();
+        assertTrue(owner.hasPermission(Permission.UPDATE_ITEM_QUANTITY));
+    }
+
+    @Test
+    public void testAddAppointment() {
+        int appointerID = -1; // -1 for founder
+        int shopID = 101;
+        owner = new Owner(appointerID, shopID);
+        owner.addManager(2, new Manager(2, shopID, new HashSet<Permission>()));
+        assertTrue(owner.getAppointments().size() == 1);
+    }
+    @Test
+    public void testRemoveAppointment() {
+        int appointerID = -1; // -1 for founder
+        int shopID = 101;
+        owner = new Owner(appointerID, shopID);
+        owner.addManager(2, new Manager(2, shopID, new HashSet<Permission>()));
+        owner.removeAppointment(2);
+        assertTrue(owner.getAppointments().size() == 0);
+    }
+    @Test
+    public void testGetAppointer() {
+        int appointerID = -1; // -1 for founder
+        int shopID = 101;
+        owner = new Owner(appointerID, shopID);
+        owner.addManager(2, new Manager(2, shopID, new HashSet<Permission>()));
+        assertEquals(-1, owner.getAppointer());
+    }
+    @Test
+    public void testGetShopID() {
+        int appointerID = -1; // -1 for founder
+        int shopID = 101;
+        owner = new Owner(appointerID, shopID);
+        owner.addManager(2, new Manager(2, shopID, new HashSet<Permission>()));
+        assertEquals(101, owner.getShopID());
+    }
+}
